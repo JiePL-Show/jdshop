4a1e7bb0b13311699ad2261906c95b8a64e14d71
JiePL-Show <zhangjie120@outlook.com>
2019-04-07 13:56:55 +0800

    first commit

---
+++ src/views/About.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <ul>
+      <li>
+        hahaha 
+      </li>
+      <li>
+        hehehe
+      </li>
+      <li>
+        heihei
+      </li>
+    </ul>
+  </div>
+</template>
